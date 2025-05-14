0ad45b59d2402daa4af30ae43cb4087defe5e544
Jason-1112 <jason.au9@studytafensw.edu.au>
2025-05-14 02:09:09 +0000

    v3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    //2
+    // let a = 1;
+    // let b = 2;
+    // let c = 3;
 
-return(
-<Jason/>
+    //3
+   
+
+    const [counter, setCounter] = useState(1); //takes in the initial state of variable
+    console.log(`App rendered ${counter}`);
+return (
+    // <>
+    // <Jason name="Jason" address="2136" />
+    // <Jason name="Cory" address="2136" />
+    // <Jason name="Sam" address="2136" />
+    // <Jason name="Jalal" address="2136" />
+    // </>
+
+    //2
+    // <>
+    // <div>{a}</div>
+    // <div>{b}</div>
+    // <div>{c}</div>
+    // </>
+
+        <>
+            <button onClick={() => { setCounter((count) => count + 1) }}>counter is {counter}</button>
+        </>
 );
 }
 
 export default App;
 
-function Jason() {
-
-<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odit itaque, dolor reiciendis sapiente eligendi tempore culpa laboriosam, nam eaque mollitia. Necessitatibus reprehenderit soluta quaerat culpa consequuntur tempora, iste eos?</div>
-}
+// function Jason(props) {
+// return(
+//     <>
+//     <div>{props.name}</div>
+//     <div>{props.address}</div>
+//     </> //mounting: an event that starts the screen.
+//         //unmounting: remove from the screen
+//         //component life cycle, mounted: show, unmounted: removed
+// )
+// }
